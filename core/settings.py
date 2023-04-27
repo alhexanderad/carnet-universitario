968715d50e3fad550c360982ac0ba44eec97f536
Alexander D. Astuhuaman <alhexanderad@gmail.com>
2023-04-26 20:17:29 -0500

    cloudinary

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,8 @@
 import os
+import cloudinary
 from pathlib import Path
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,45 +14,44 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'estudiante',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+
+  'cloudinary',
+  'estudiante',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [BASE_DIR / 'templates'],
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
     },
+  },
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
@@ -59,10 +61,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_uncp_carnet.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db_uncp_carnet.sqlite3',
+  }
 }
 
 
@@ -70,18 +72,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
 
 
@@ -103,11 +105,22 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
   BASE_DIR / 'static',
-]
+  ]
+#Formato de Fecha
+DATE_INPUT_FORMATS = ['%d%m%Y']
+DATETIME_INPUT_FORMATS = ['%d%m%Y']
 
 # insta_project/settings.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Config
+cloudinary.config(
+  cloud_name = "dnlulav1o",
+  api_key = "346666852897273",
+  api_secret = "IBG_0aqyDFU_G1kJef1zBBDe6Og",
+  secure = True
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- estudiante/models.py
+++ estudiante/models.py
@@ -1,9 +1,8 @@
+import cloudinary.uploader
 from django.db import models
 from django.core.validators import RegexValidator
+from cloudinary.models import CloudinaryField
 
-
-def user_path(instance, filename):
-  return '1_{0}/{1}'.format(instance.user.id, filename)
 class Estudiante(models.Model):
   DOCUMENTO =(
     ('1','Documento Nacional de Identidad'),
@@ -21,19 +20,17 @@ class Estudiante(models.Model):
   tipo_documento = models.CharField(max_length=100, choices=DOCUMENTO, default='Carné de Identidad')
   dni_regex = RegexValidator(regex=r'^\+?1?\d{8}', message = ("Debe de ingresar los 8 digitos de DNI"))
   documento_1 = models.CharField(validators=[dni_regex], max_length=8, unique=True)
-  #documento_2 = models.CharField(validators=[dni_regex], max_length=9, unique=True)
-  #pasporte = models.CharField(max_length=9, unique=True)
   codigo_matricula = models.CharField(max_length=15, unique=True)
   email_est = models.EmailField()
   telefono_regex = RegexValidator(regex=r'^\+?1?\d{9,15}', message = ("Debe de ingresar el numero de 9 digitos"))
   telefono_est = models.CharField(validators=[telefono_regex], max_length=9, blank=False)
-  foto = models.ImageField(upload_to='images/user_path', blank=True, null=True)
-  #imgdni_est = models.ImageField(blank=True, null=True)
-  #imgfoto_est = CloudinaryField('image',blank=True, null=True)
-  #imgdni_est = CloudinaryField('image',blank=True, null=True)
+  foto = CloudinaryField('image', default='media/default-img.jpg')
   fecha_ingreso = models.DateTimeField(auto_now_add=True)
   fecha_actualizar = models.DateTimeField(auto_now=True)
 
+  def save(self, *args, **kwargs):
+    cloudinary.uploader.upload(self.foto, folder="carnet-universitario", public_id='1_{}'.format(self.documento_1))
+    super().save(*args, **kwargs)
   class Meta:
     get_latest_by = 'fecha_ingreso'
 
